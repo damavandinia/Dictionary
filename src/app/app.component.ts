4dee277f1cd06affc3be6b47b55c0fb26ca06991
sobhan <damavandisobhan@gmail.com>
2022-11-20 09:18:19 +0330

    change main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,127 +1,17 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {Word} from "./word-item/word.model";
-import {WordListService} from "./word-list.service";
-import {Subscription} from "rxjs";
-import {animate, state, style, transition, trigger} from "@angular/animations";
-import {MatSnackBar} from "@angular/material/snack-bar";
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "./auth/auth.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  animations: [
-    trigger('modal', [
-      state('show', style({
-        opacity: 1
-      })),
-      transition('void => *' , [
-        style({
-          opacity: 0
-        }),
-        animate(200)
-    ]),
-      transition('* => void' , [
-        animate(200 , style({
-          opacity: 0
-        }))
-      ])
-    ]),
-    trigger('listAnim', [
-      state('show', style({
-        opacity: 1,
-        transform: 'translateX(0)'
-      })),
-      transition('void => *' , [
-        style({
-          opacity: 0,
-          transform: 'translateX(-10px)'
-        }),
-        animate(150)
-      ]),
-      transition('* => void' , [
-        animate(150 , style({
-          transform: 'translateX(10px)',
-          opacity: 0
-        }))
-      ])
-    ])
-  ]
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy{
+export class AppComponent implements OnInit{
 
-  private wordChangedSub: Subscription;
-  private openEditModal: Subscription;
-  private serverError: Subscription;
-  private startProgress: Subscription;
-  showModal = false;
-  modalData: any;
+  constructor(private authService: AuthService) {}
 
-  words: Word[];
-  wordEditItem: Word;
-
-  showProgress = true;
-
-  filterTxt = '';
-  sortType = '1';
-  filterType = 'All';
-
-  constructor(private wordService: WordListService,
-              private _snackBar: MatSnackBar) {}
-
-  ngOnInit() {
-    this.words = this.wordService.getWords();
-    this.wordChangedSub = this.wordService.wordsChanged.subscribe(
-      (words: Word[]) => {
-        this.showProgress = false;
-        this.words = words;
-      }
-    )
-
-    this.openEditModal = this.wordService.openEditWord.subscribe(
-      (id: number) => {
-
-        this.wordEditItem = this.wordService.getWordItem(id);
-        this.modalData = { title: 'Edit Word', buttonTitle: 'Edit', id: id , editItem: this.wordEditItem};
-        this.showModal = true;
-      }
-    )
-
-    this.serverError = this.wordService.serverError.subscribe(
-      (errorMessage: string) => {
-        this.showProgress = false;
-        this._snackBar.open(errorMessage, null, {"duration": 4000});
-      }
-    )
-
-    this.startProgress = this.wordService.startProgress.subscribe(
-      () => {
-        this.showProgress = true;
-      }
-    )
-
-    this.wordService.fetchWords();
+  ngOnInit(): void {
+    this.authService.autoLogin();
   }
 
-  getFilterTxt(filterData: {text: string , sort: string , filter: string}) {
-    this.filterTxt = filterData.text;
-    this.sortType = filterData.sort;
-    this.filterType = filterData.filter;
-  }
-
-  openModal(isNew: boolean, title: string, btnTitle: string){
-
-    this.modalData = { title: title, buttonTitle: btnTitle , editItem: new Word(0 , '' , '')};
-    this.showModal = true;
-  }
-
-  closeModal(){
-    this.showModal = false;
-  }
-
-  ngOnDestroy(): void {
-    this.wordChangedSub.unsubscribe();
-    this.openEditModal.unsubscribe();
-    this.serverError.unsubscribe();
-    this.startProgress.unsubscribe();
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,8 @@ import {MatSelectModule} from "@angular/material/select";
 import {MatMenuModule} from "@angular/material/menu";
 import {MatButtonModule} from "@angular/material/button";
 import {MatSnackBarModule} from "@angular/material/snack-bar";
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -15,15 +16,32 @@ import { HeaderComponent } from './header/header.component';
 import { WordItemComponent } from './word-item/word-item.component';
 import { FilterPipe } from './filter.pipe';
 import { ModalComponent } from './modal/modal.component';
-import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import { LoginComponent } from './login/login.component';
+import {AppRoutingModule} from "./app.routing.module";
+import {DictionaryComponent} from "./dictionary/dictionary.component";
+import {AuthInterceptorService} from "./auth/auth-interceptor.service";
+import {AngularFireModule} from "@angular/fire/compat";
+import {AngularFireAuthModule} from "@angular/fire/compat/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCazdiRoo9M2hfSRaSGgDZ3hghtYeRrM4M",
+  authDomain: "dictionary-90a42.firebaseapp.com",
+  databaseURL: "https://dictionary-90a42-default-rtdb.firebaseio.com",
+  projectId: "dictionary-90a42",
+  storageBucket: "dictionary-90a42.appspot.com",
+  messagingSenderId: "569119452129",
+  appId: "1:569119452129:web:71afc98e763074d40440a0"
+}
 
 @NgModule({
   declarations: [
     AppComponent,
+    DictionaryComponent,
     HeaderComponent,
     WordItemComponent,
     FilterPipe,
-    ModalComponent
+    ModalComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -37,9 +55,16 @@ import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
     ReactiveFormsModule,
     MatSnackBarModule,
     HttpClientModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
