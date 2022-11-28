ea2fd5b5175d32ba2407d5d9abf4da01cb8f000d
sobhan <damavandisobhan@gmail.com>
2022-11-28 18:51:58 +0330

    set and check for dark theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,69 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, HostBinding, OnDestroy} from '@angular/core';
 import {AuthService} from "./auth/auth.service";
+import {OverlayContainer} from "@angular/cdk/overlay";
+import {ThemeService} from "./theme.service";
+import {Subscription} from "rxjs";
+
+const THEME_DARKNESS_SUFFIX = `-dark`;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, OnDestroy{
 
-  constructor(private authService: AuthService) {}
+  @HostBinding('class') currentThemeCssClass: string;
+  isThemeDark = false;
+  currentTheme: string;
+  private darkModeToggleSub: Subscription;
+
+  constructor(private authService: AuthService,
+              private overlayContainer: OverlayContainer,
+              private themeService: ThemeService) {
+
+    // if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    //   this.isThemeDark = true;
+    // }
+
+    let isDark = JSON.parse(localStorage.getItem('isThemeDark'));
+    this.setTheme('indigo-pink', isDark); // Default Theme
+
+    this.darkModeToggleSub = this.themeService.toggleDarkMode.subscribe(
+      (mode: boolean) => {
+        this.setTheme(this.currentTheme, mode);
+      }
+    )
+  }
 
   ngOnInit(): void {
     this.authService.autoLogin();
   }
 
+  setTheme(theme: string, darkness: boolean = null) {
+    if (darkness === null) {
+      darkness = this.isThemeDark;
+    }else if (this.isThemeDark === darkness) {
+      if (this.currentTheme === theme) return;
+    } else {
+      this.isThemeDark = darkness;
+      localStorage.setItem("isThemeDark" , JSON.stringify(this.isThemeDark));
+    }
+
+    this.currentTheme = theme;
+
+    const cssClass = darkness === true ? theme + THEME_DARKNESS_SUFFIX : theme;
+
+    const classList = this.overlayContainer.getContainerElement().classList;
+    if (classList.contains(this.currentThemeCssClass))
+      classList.replace(this.currentThemeCssClass, cssClass);
+    else
+      classList.add(cssClass);
+
+    this.currentThemeCssClass = cssClass;
+  }
+
+  ngOnDestroy() {
+    this.darkModeToggleSub.unsubscribe();
+  }
 }
